78b8bb0a884c2ff1fa9afc34d250cb8daa2ba89f
taavi <taaw88@gmail.com>
2022-03-02 18:19:08 +0200

    henkiloluokan luonti

--- GitExercise.py
+++ GitExercise.py
@@ -1,2 +1,24 @@
-def hello():
-    return
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        self._name = value
+
+    @property
+    def age(self):
+        return self._age
+
+    @age.setter
+    def age(self, value):
+        self._age = value
+
+
+    def __str__(self):
+        return f'Nimi: {self._name}\nIka: {self._age}\n'
